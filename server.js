caa4face44269ce58e2012c3568d4fb50568468a
RiceGnat <ricegnat@gmail.com>
2020-03-05 15:33:53 -0600

    added ui to server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ sys.initialize();
 express().use(cors()).get("/sys", async (req, res) => {
     res.send(await sys.get())
 })
+.use("/", express.static("build"))
 .listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
